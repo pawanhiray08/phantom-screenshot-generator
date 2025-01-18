df425dea6f52cf823009e3b8fe47bedd43851b2e
pawanhiray08 <pawanhiray88@gmail.com>
2025-01-18 13:49:31 +0530

    Update Vite config and image handling for better deployment support

--- src/components/TransactionPreview.tsx
+++ src/components/TransactionPreview.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Button } from "@/components/ui/button";
-import phantomSendImage from '../images/phantom-send.png';
+import phantomSendImage from './phantom-send.png';
 
 interface TransactionPreviewProps {
   data: {
